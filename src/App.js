67be83def0e6055db25010aaed0b77153e6cf2fa
Dopam1ne <roywhoa@gmailcom>
2018-07-19 14:01:18 -0700

    fixed game logic

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ let clicked = [];
 let score = 0;
 let high = 0;
  class App extends Component {
-  
   state = {
     score,
     images,
     high,
   };
-
+//end game function sets high score and clears score and clicked array
   endGame = () =>{
     alert("Game Over");
     if(this.state.high<this.state.score){
       this.setState({high:this.state.score});
+    }
       clicked=[];
       this.setState({score:0})
-    }
     
   };
-  
+  //shuffles images
   shuffle = (array)=>{
     let i = array.length-1;
     for (;i>0;i--){
@@ -34,21 +33,21 @@ let high = 0;
     };
     return array;
   };
-
+//click 
   imgClick = (id) =>{
     this.setState({images:this.shuffle(this.state.images)})
-    
-    if(this.state.score % 16 ===0){
+//resests onced clicked to continue once all have been clikced
+    if(this.state.score % 25 === 0){
       clicked=[]
-    }
-    
+    };    
+//checks to see if img id is NOT in clicked array
     if(clicked.indexOf(id) === -1){
       this.setState({score:this.state.score+1}); 
       clicked.push(id);
+//ends game if img id is in clicked array
     }else{
     this.endGame();
    }
-  
   };
 
 
